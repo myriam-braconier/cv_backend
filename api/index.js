59b717e5b388f6c060310df8b00452129abe510c
volubyl01 <myriam.braconier@gmail.com>
2024-12-30 18:38:19 +0100

    index.js

--- api/index.js
+++ api/index.js
@@ -2,18 +2,11 @@ import express from 'express';
 import db from '../models/index.js';
 import app from '../app.js';
 
-const app = express();
-app.use(express.json());
 
 // Vérifier que db est correctement importé
 console.log('DB Status:', {
     isConnected: db.sequelize?.authenticate ? true : false,
     models: Object.keys(db)
-});
-
-// Configuration des routes
-app.get('/api', (req, res) => {
-    res.json({ message: 'API fonctionne' });
 });
 
 

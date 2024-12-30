5e5aef395c6c76b8d3418f1855f5ecb3e316ce61
volubyl01 <myriam.braconier@gmail.com>
2024-12-30 18:54:28 +0100

    config index

--- api/index.js
+++ api/index.js
@@ -1,18 +1,4 @@
-
-import db from '../models/index.js';
 import app from '../app.js';
-import express from 'express';
 
-
-// Vérifier que db est correctement importé
-console.log('DB Status:', {
-    isConnected: db.sequelize?.authenticate ? true : false,
-    models: Object.keys(db)
-});
-
-
-// Export pour Vercel
-export default function handler(req, res) {
-    return app(req, res);
-}
+export default app;
 

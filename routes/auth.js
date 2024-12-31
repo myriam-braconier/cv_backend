a5b926304c91d8176cec8dc3761944fefd1cd1e8
volubyl01 <myriam.braconier@gmail.com>
2024-12-31 13:16:41 +0100

    auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
-router.post('/verify', authController.verify);
+router.post('/verify', authenticateToken,authController.verify);
 
 
 export default router;

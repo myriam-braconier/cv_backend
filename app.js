cb3c2a8f88f09753c278990644843adf35b00bf6
volubyl01 <myriam.braconier@gmail.com>
2025-01-01 17:46:56 +0100

    websocket echec

--- app.js
+++ app.js
@@ -134,6 +134,6 @@ const PORT = process.env.NODE_ENV === 'development' ? 4000 : 3306;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-initWebSocket();
+
 
 export default app;

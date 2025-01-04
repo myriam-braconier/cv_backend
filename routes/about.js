19d58cc79d5a83b968cf512e73c26d801b204f63
volubyl01 <myriam.braconier@gmail.com>
2025-01-04 19:40:48 +0100

    about

--- routes/about.js
+++ routes/about.js
@@ -1,8 +1,16 @@
-import express from "express";
+import express from 'express';
+import About from '../models/about.js';
+
 const router = express.Router();
 
-router.get("/about", (req, res) => {
-	res.status(200).send("Page About");
+router.get('/about', async (req, res) => {
+  try {
+    const aboutData = await About.findAll();
+    res.status(200).json(aboutData);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données About:', error);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
 });
 
-export default About
+export default router;
